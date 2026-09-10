23ebfad51db85788a14d93b4ef36d6ce52ac6abd
saimmist-tech <saimmist@gmail.com>
2026-09-10 17:41:43 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ except Exception as _fe:
 # ==========================================
 # Configuration (Token & Owner ID)
 # ==========================================
-TOKEN = "8510677584:AAF5t0ZcQStcooraULvtULzSAtIT7pZ4ZOo"
+TOKEN = "8510677584:AAEEUUbHL_mjvrLUaPaj9_fFAh2SUPNzrL8"
 BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
 FILE_URL = f"https://api.telegram.org/file/bot{TOKEN}/"
 
